a53eb09fd1de4c882883f4c3aa28ea8b848ebb7c
Linh Bui-Raborn <linhbui2811@gmail.com>
2026-05-07 10:58:23 -0400

    Update clinsv.sh

--- codes/clinsv.sh
+++ codes/clinsv.sh
@@ -8,30 +8,24 @@ ulimit -s 10240
 
 module load clinsv/1.1
 
-workdir_remote="/lscratch/$SLURM_JOB_ID/${sample}"
-workdir=/data/buirabornlt/Melanoma_family/WGS/Calling/ClinSV
-bamdir=/data/buirabornlt/WGS_MelaNostrum/WGS/Bam_file/
+workdir_remote="/lscratch/$SLURM_JOB_ID/"
+workdir='/path/to/working/dir'
+bamdir='/path/to/bam/file/dir'
+bamfile='name/bamfile'
 refdata_path=/usr/local/apps/clinsv/ref_hg38/clinsv/refdata-b38
 
-mkdir -p $workdir/$sample
 mkdir -p $workdir_remote
 
-cd $workdir/$sample
-
-# Copy cram files and convert to bam
-#rsync $cramfile ./
-#samtools view -T /data/buirabornlt/Reference/Homo_sapiens_assembly38.fasta -b -o $sample.bam $cramfile
-#samtools index $sample.bam
+cd $workdir
 
 # Run ClinSV
-# clinsv -p $workdir/$sample -i $sample.bam
 singularity run --bind $refdata_path:/app/ref-data/refdata-b38 \
-	    --bind $project_folder:$workdir/$sample \
+	    --bind $project_folder:$workdir \
 	    --bind $input_path:$bamdir \
 	    /usr/local/apps/clinsv/1.1/libexec/1.1.sif \
         /app/clinsv/bin/clinsv \
 	    -r all \
-	    -p $workdir/$sample  \
-	    -i $bamdir/$sample.bam \
+	    -p $workdir  \
+	    -i $bamdir/$bamfile \
 	    -ref /app/ref-data/refdata-b38 \
 	    -eval yes
